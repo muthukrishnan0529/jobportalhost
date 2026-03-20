6d8a5d27d19bd31cd547b7313cdd745a82e1d780
muthukrishnan0529 <muthukrishnan0529@gmail.com>
2026-03-20 16:53:18 +0530

    fix admin user role field

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,50 +1,49 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 
-@admin.register(User)
-class UserAdmin(BaseUserAdmin):
+class UserAdmin(admin.ModelAdmin):
 
     list_display = (
         "id",
         "username",
         "email",
         "role",
-        "phone",
-        "company_name",
         "is_profile_completed",
-        "is_staff",
     )
 
-    list_filter = (
-        "role",
-        "is_profile_completed",
-        "is_staff",
-        "is_superuser",
+    fieldsets = (
+        (None, {
+            "fields": ("username", "password")
+        }),
+
+        ("Personal Info", {
+            "fields": (
+                "email",
+                "phone",
+                "resume",
+            )
+        }),
+
+        ("Role Info", {
+            "fields": (
+                "role",
+                "company_name",
+                "designation",
+                "is_profile_completed",
+            )
+        }),
+
+        ("Permissions", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        }),
     )
 
-    search_fields = (
-        "username",
-        "email",
-        "phone",
-        "company_name",
-    )
 
-    ordering = ("-id",)
-
-    fieldsets = BaseUserAdmin.fieldsets + (
-        (
-            "Extra Info",
-            {
-                "fields": (
-                    "role",
-                    "phone",
-                    "resume",
-                    "company_name",
-                    "designation",
-                    "is_profile_completed",
-                )
-            },
-        ),
-    )
+admin.site.register(User, UserAdmin)
